c888e834fa68c0f1060f9e5b2efbf6517a42cc30
jollaman999 <admin@jollaman999.com>
2023-07-19 10:19:07 +0900

    systemctl: Check root permission

--- systemctl/controlService.go
+++ systemctl/controlService.go
@@ -2,11 +2,17 @@ package systemctl
 
 import (
 	"github.com/jollaman999/utils/cmd"
+	"github.com/jollaman999/utils/syscheck"
 )
 
 // RestartService : Restart the service with systemctl command.
 func RestartService(serviceName string) error {
-	err := cmd.RunCMD("systemctl" + serviceName + "restart")
+	err := syscheck.CheckRoot()
+	if err != nil {
+		return err
+	}
+
+	err = cmd.RunCMD("systemctl" + serviceName + "restart")
 	if err != nil {
 		return err
 	}

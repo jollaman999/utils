2717f97bdfd36238497462c62fe4b0c40ec6072c
jollaman999 <admin@jollaman999.com>
2023-07-19 10:58:07 +0900

    syscheck: Add a function of increasing RLimit

---
+++ syscheck/checkRLimit.go
@@ -0,0 +1,23 @@
+package syscheck
+
+import "syscall"
+
+// IncreaseRLimitToMax : Increase rLimit to system max
+func IncreaseRLimitToMax() error {
+	var rLimit syscall.Rlimit
+
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		return err
+	}
+
+	if rLimit.Cur < rLimit.Max {
+		rLimit.Cur = rLimit.Max
+		err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}

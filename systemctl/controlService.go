f661c4544ecf38b8435a0efa7484ab35cff4ba2a
jollaman999 <admin@jollaman999.com>
2023-07-19 10:24:58 +0900

    systemctl: Add more options to control the service

--- systemctl/controlService.go
+++ systemctl/controlService.go
@@ -5,17 +5,36 @@ import (
 	"github.com/jollaman999/utils/syscheck"
 )
 
-// RestartService : Restart the service with systemctl command.
-func RestartService(serviceName string) error {
+func controlServiceCommon(serviceName string, option string) error {
 	err := syscheck.CheckRoot()
 	if err != nil {
 		return err
 	}
 
-	err = cmd.RunCMD("systemctl" + serviceName + "restart")
+	err = cmd.RunCMD("systemctl" + serviceName + option)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+// StartService : Start the service with systemctl command.
+func StartService(serviceName string) error {
+	return controlServiceCommon(serviceName, "start")
+}
+
+// StopService : Stop the service with systemctl command.
+func StopService(serviceName string) error {
+	return controlServiceCommon(serviceName, "stop")
+}
+
+// RestartService : Restart the service with systemctl command.
+func RestartService(serviceName string) error {
+	return controlServiceCommon(serviceName, "restart")
+}
+
+// CustomOption : Run systemctl command with the custom option.
+func CustomOption(serviceName string, option string) error {
+	return controlServiceCommon(serviceName, option)
 }

412b7c9fd5030380e3a84c60aa6cca9b73f767bb
jollaman999 <admin@jollaman999.com>
2023-12-07 10:19:27 +0900

    syscheck: Add support of running with administrator privilege in Windows

--- syscheck/checkRoot_windows.go
+++ syscheck/checkRoot_windows.go
@@ -6,9 +6,45 @@ package syscheck
 
 import (
 	"errors"
+	"golang.org/x/sys/windows"
+	"os"
+	"strings"
+	"syscall"
 )
 
-// CheckRoot : Check root permission
+func runAsAdministrator() error {
+	verb := "runas"
+	exe, _ := os.Executable()
+	cwd, _ := os.Getwd()
+	args := strings.Join(os.Args[1:], " ")
+
+	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+	exePtr, _ := syscall.UTF16PtrFromString(exe)
+	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+	argPtr, _ := syscall.UTF16PtrFromString(args)
+
+	var showCmd int32 = 1 //SW_NORMAL
+
+	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func amAdmin() bool {
+	return windows.GetCurrentProcessToken().IsElevated()
+}
+
+// CheckRoot : Run as Administrator in Windows with UAC
 func CheckRoot() error {
-	return errors.New("checking root is not supported on Windows")
+	if !amAdmin() {
+		err := runAsAdministrator()
+		if err != nil {
+			return errors.New("Failed to run as administrator. (" + err.Error() + ")")
+		}
+	}
+
+	return nil
 }
